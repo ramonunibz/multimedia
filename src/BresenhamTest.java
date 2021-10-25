c97bde5f1b59524b71d692cc931e36d207a79fe2
ramon <rpiha@unibz.it>
2021-10-25 11:52:14 +0200

    added src folder

---
+++ src/BresenhamTest.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class BresenhamTest {
+	
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Bresenham");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(600, 600);
+		BresenhamCanvas bresenhamCanvas = new BresenhamCanvas(25, 25);
+		frame.getContentPane().add(bresenhamCanvas);
+		frame.setVisible(true);
+	}
+}
